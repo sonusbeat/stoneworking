e91fe3a219853fe3e751cb093d5158dbe8bc60f7
Daniel González Briseño <sonusbeat@hotmail.com>
2016-11-21 15:10:26 -0600

    Se modificó la plantilla master

--- resources/views/templates/main.blade.php
+++ resources/views/templates/main.blade.php
@@ -14,19 +14,23 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
         <!-- Custom Fonts -->
-        <!-- <link rel="stylesheet" href="css/fontastic.css"> -->
         <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet'>
         <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet'>
-
-        <!-- Plugins CSS -->
-        <link href="/css/jquery.bxslider.css" rel="stylesheet" />
-        <!-- <link href="/css/magnific-popup.css" rel="stylesheet"> -->
 
         <!-- Theme CSS -->
         <link href="/css/main.min.css" rel="stylesheet">
 
         <!-- Libraries CSS -->
         <link href="/css/libraries.min.css" rel="stylesheet">
+
+        <!-- jQuery -->
+        <script src="http://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
+
+        <!-- Bootstrap Core JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+        <!-- JavaScript Plugins -->
+        <script src="js/libraries.min.js"></script>
 
         <!--[if lt IE 9]>
             <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
@@ -39,15 +43,6 @@
         @include('public/partials/navigation')
 
         @yield('content')
-
-        <!-- jQuery -->
-        <script src="http://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
-
-        <!-- Bootstrap Core JavaScript -->
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-
-        <!-- JavaScript Plugins -->
-        <script src="js/libraries.min.js"></script>
 
         <!-- Custom Scripts -->
         @yield('custom-scripts')
