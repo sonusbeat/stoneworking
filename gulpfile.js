19921bc7197fc0d3036d584b1442c896a7eae7d7
Daniel González Briseño <sonusbeat@hotmail.com>
2016-11-25 22:57:48 -0600

    Se modificó el bxslider

--- gulpfile.js
+++ gulpfile.js
@@ -3,22 +3,22 @@ var elixir = require('laravel-elixir');
 
 elixir(function(mix) {
 	/* ################## Public ################## */
-   mix.sass('main.scss', 'public/css/main.min.css');
+  mix.sass('main.scss', 'public/css/main.min.css');
 
-    // mix.styles([
-    // 	'fontastic.css',
-    //     'magnific-popup.css',
-    // ], 'public/css/libraries.min.css');
+  // mix.styles([
+  // 	'fontastic.css',
+  //     'magnific-popup.css',
+  // ], 'public/css/libraries.min.css');
 
-    // mix.scripts([
-    // 	'scrollreveal.min.js',
-    // 	'jquery.magnific-popup.min.js',
-    //  'jquery.goup.js',
-    //  'jquery.easing.min.js',
-    // 	'creative.min.js',
-    // ], 'public/js/libraries.min.js');
+  // mix.scripts([
+  // 	'scrollreveal.min.js',
+  // 	'jquery.magnific-popup.min.js',
+  //  'jquery.goup.js',
+  //  'jquery.easing.min.js',
+  // 	'creative.min.js',
+  // ], 'public/js/libraries.min.js');
     
-    mix.browserSync({
-        proxy: 'www.stoneworking.dev'
-    });
+  mix.browserSync({
+      proxy: 'stoneworking.dev'
+  });
 });
